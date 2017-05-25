2cbaf51ef633c05b2400a6ce40e8cce605fe135f
Mariusz558 <joga55@poczta.fm>
2017-05-25 22:58:36 +0200

    initial commit

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i = 1;
+            if (i = 1)
+                return true;
+        }
+    }
+}
